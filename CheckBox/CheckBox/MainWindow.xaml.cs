4fbed48ed937f56744be1039a207d1d76d75defe
shivam <60393209+shivam18254@users.noreply.github.com>
2021-11-15 15:09:01 +0530

    commit

---
+++ CheckBox/CheckBox/MainWindow.xaml.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace CheckBox
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        private void cbAllCheckedChanged(object sender, RoutedEventArgs e)
+        {
+            bool newval = (cbAllTopings.IsChecked == true);
+            cbSalani.IsChecked = newval;
+            cbMushrooms.IsChecked = newval;
+            cbMozzarella.IsChecked = newval;
+        }
+        private void cbSingleCheckedChnged(object sender, RoutedEventArgs e)
+        {
+            cbAllTopings.IsChecked = null;
+            if((cbSalani.IsChecked==true) && (cbMozzarella.IsChecked==true)&&(cbMushrooms.IsChecked==true))
+                    {
+                cbAllTopings.IsChecked = true;
+            }
+            if ((cbSalani.IsChecked == false) && (cbMozzarella.IsChecked == false) && (cbMushrooms.IsChecked == false))
+            {
+                cbAllTopings.IsChecked = false;
+            }
+        }
+    }
+}
