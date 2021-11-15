9cb637b12330842138e842aafffa59e70b5a069e
shivam <60393209+shivam18254@users.noreply.github.com>
2021-11-15 15:14:33 +0530

    commit

---
+++ Linq2/Linq2/Program.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Linq2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            UniversityManager um = new UniversityManager();
+            um.MaleStudents();
+            um.FemaleStudents();
+            Console.ReadKey();
+        }
+    }
+    class UniversityManager
+    {
+        public List<University> universities;
+        public List<Student> students;
+
+        public UniversityManager()   //constructor
+        {
+            universities = new List<University>();
+            students = new List<Student>();
+
+            //add university
+            universities.Add(new University { Id = 1, Nmae = "Graphic Era Deemed University" });
+            universities.Add(new University { Id = 2, Nmae = "Graphic Era Hill University" });
+
+
+            //add students
+            students.Add(new Student { Id = 1, Name = "Shivam", Gender = "male", Age = 21, UniversityId = 1 });
+            students.Add(new Student { Id = 2, Name = "Rohan", Gender = "male", Age = 21, UniversityId = 2 });
+            students.Add(new Student { Id = 3, Name = "Ram", Gender = "male", Age = 21, UniversityId = 1 });
+            students.Add(new Student { Id = 4, Name = "Ravi", Gender = "male", Age = 21, UniversityId = 2 });
+            students.Add(new Student { Id = 5, Name = "Geeta", Gender = "female", Age = 21, UniversityId = 1 });
+            students.Add(new Student { Id = 6, Name = "Sita", Gender = "female", Age = 21, UniversityId = 2 });
+        }
+        public void MaleStudents()
+        {
+            IEnumerable<Student> mailStudents = from Student in students where Student.Gender == "male" select Student;
+            Console.WriteLine("Male student: ");
+
+            foreach (Student student in mailStudents)
+            {
+                student.Print();
+            }
+
+        }
+        public void FemaleStudents()
+        {
+            IEnumerable<Student> femailStudents = from Student in students where Student.Gender == "female" select Student;
+            Console.WriteLine("female student: ");
+
+            foreach (Student student in femailStudents)
+            {
+                student.Print();
+            }
+        }
+    }
+
+
+    class University
+    {
+        public int Id { get; set; }
+        public string Nmae { get; set; }
+
+
+        public void Print()
+        {
+            Console.WriteLine("University {0} with id is {1} ", Nmae, Id);
+        }
+    }
+
+    class Student
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Gender { get; set; }
+        public int Age { get; set; }
+
+        public int UniversityId { get; set; }
+        public void Print()
+        {
+            Console.WriteLine("Student {0} with Id {1},Gender {2}" +
+                "nad Age {3} fro University with the Id {4}", Name
+                , Id, Gender, Age, UniversityId);
+        }
+
+    }
+}
---
+++ lists/lists/MainWindow.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace lists
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            List<Match> matches = new List<Match>();
+            matches.Add(new Match() { Team1 = "India", Team2 = "Shrilanka", Score1 = 1, Score2 = 2, Completion = 20 });
+            Matches.ItemsSource = matches;
+         
+        }
+
+        public class Match
+        {
+            public int Score1 { get; set; }
+            public int Score2 { get; set; }
+            public string Team1 { get; set; }
+            public string Team2 { get; set; }
+            public int Completion { get; set; }
+        }
+    }
+}
